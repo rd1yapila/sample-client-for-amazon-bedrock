e80e4b0b83a11c4393a7fac1e400d09fcb416bc9
rd1yapila <a.lipay.1.dr@gmail.com>
2025-05-31 10:29:14 +0800

    Update constant.ts to support claude-4-opus

--- app/constant.ts
+++ app/constant.ts
@@ -182,6 +182,6 @@ export const DEFAULT_MODELS = [
   },
 ] as const;
 
-export const REASONING_MODEL = ["claude-3.7-sonnet"];
+export const REASONING_MODEL = ["claude-3.7-sonnet","claude-4-opus"];
 export const CHAT_PAGE_SIZE = 15;
 export const MAX_RENDER_MSG_COUNT = 45;
